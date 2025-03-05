807c8bfda300fed98ec51651ff11d326f5f403e8
sofiailse <sofiailsefp@gmail.com>
2025-03-05 18:50:04 +0100

    Use cartContext

--- src/app/store/Store.tsx
+++ src/app/store/Store.tsx
@@ -1,13 +1,23 @@
+import { useContext } from "react";
 import Search from "../search/Search";
 import ShoppingCart from "../shoppingCart/ShoppingCart";
 import "./store.css";
+import { CartContext } from "../context/CartContext";
 
 const Store = () => {
+  const cartContext = useContext(CartContext);
+
+  if (!cartContext) {
+    throw new Error("MyComponent must be used within a CartProvider");
+  }
+
+  const { cart, setCart } = cartContext;
+
   return (
     <div className="store-container">
       <h1>Tienda - El topo</h1>
       <Search />
-      <ShoppingCart />
+      <ShoppingCart cart={cart} setCart={setCart} />
     </div>
   );
 };
