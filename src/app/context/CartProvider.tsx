79e6045bea7651f9c56e0933cb4dc3032fb73f48
sofiailse <sofiailsefp@gmail.com>
2025-03-05 19:08:55 +0100

    Restore cart from local storage when component loads. Save cart on local storage when context changes.

--- src/app/context/CartProvider.tsx
+++ src/app/context/CartProvider.tsx
@@ -1,8 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CartContext } from "./CartContext";
+import {
+  getCartFromLocalStorage,
+  saveCartOnLocalStorage,
+} from "../utils/localStorage/cart";
 
 const CartProvider = ({ children }: { children: any }) => {
   const [cart, setCart] = useState(null);
+
+  useEffect(() => {
+    const savedCart = getCartFromLocalStorage();
+
+    if (savedCart) setCart(JSON.parse(savedCart));
+  }, []);
+
+  useEffect(() => {
+    if (cart) saveCartOnLocalStorage(cart);
+  }, [cart]);
 
   return (
     <CartContext.Provider value={{ cart, setCart }}>
